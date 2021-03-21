6272ee06390965e8b93fc5c1c6f1d09d43d311bf
Javier Teso <javier@kumologica.com>
2021-03-21 16:57:18 +1100

    main flow repositioning working on best case scenario

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
 const dagre = require('dagre')
 
 const NODE_SIZE = 45;
-const NODE_PROPS = { label: 'example of label', width: NODE_SIZE, height: NODE_SIZE }
-// Create a new directed graph 
-let g= new dagre.graphlib.Graph();
 
-// Set an object for the graph label
-g.setGraph({rankdir: "LR", nodesep: 100, marginx: 50, marginy: 50 });
+function layouter(flow) {
+    // Create a new directed graph 
+    let maing= new dagre.graphlib.Graph();
 
-// Default to assigning a new object as a label for each new edge.
-g.setDefaultEdgeLabel(function() { return {}; });
+    // Set an object for the graph label
+    maing.setGraph({rankdir: "LR", nodesep: 100, marginx: 50, marginy: 50 });
 
-// Add nodes
-g.setNode('EventListener', { label: 'example of label', width: NODE_SIZE, height: NODE_SIZE }); // EventListener
-g.setNode('Logger', { label: 'example of label', width: NODE_SIZE, height: NODE_SIZE }); // Logger
-g.setNode('SetProperty', { label: 'example of label', width: NODE_SIZE, height: NODE_SIZE });  // Set Property
-g.setNode('Function', { label: 'example of label', width: NODE_SIZE, height: NODE_SIZE });  // Function
-g.setNode('EventListenerEnd', { label: 'example of label', width: NODE_SIZE, height: NODE_SIZE });  // EventListenerEnd
+    // Default to assigning a new object as a label for each new edge.
+    maing.setDefaultEdgeLabel(function() { return {}; });
 
-// Add edges to the graph.
-g.setEdge("EventListener",   "Logger");
-g.setEdge("Logger", "SetProperty");
-g.setEdge("SetProperty",     "Function");
-g.setEdge("Function",     "EventListenerEnd");
+    let mainNodes = getNodes('main.flow', flow);
 
-dagre.layout(g);
+    // Create nodes for the main graph
+    mainNodes.forEach(n => {
+        maing.setNode(n.id, {label: n.name, width: NODE_SIZE, height: NODE_SIZE })
+    });
 
-// Printing results
-g.nodes().forEach(function(v) {
-    console.log("Node " + v + ": " + JSON.stringify(g.node(v)));
-});
-// console.log('------')
-// g.edges().forEach(function(e) {
-//     console.log("Edge " + e.v + " -> " + e.w + ": " + JSON.stringify(g.edge(e)));
-// });
+    // Create the edges for the main graph
+    mainNodes.forEach(n => {
+        let source = n.id;
+        let target;
+        n.wires.forEach(e => {
+            target = e[0];
+            maing.setEdge(source, target)
+        })
+    });
+
+    dagre.layout(maing);
+
+    // replace the coordinates of the original flow
+    maing.nodes().forEach(nid => {
+        if (nid){
+            console.log(`Applying positioning to ${nid}...`);
+            let node = flow.find(n => n.id === nid);
+            if (node) {
+                node.x = maing.node(nid).x;
+                node.y = maing.node(nid).y;
+                // console.log('Original node repositioned to: ', JSON.stringify(node));
+            }
+        }
+    });
+
+    console.log(JSON.stringify(flow));
+    
+   
+    // Printing results
+    // maing.nodes().forEach(function(v) {
+    //     console.log("Node " + v + ": " + JSON.stringify(maing.node(v)));
+    // });
+    // console.log('------')
+    // g.edges().forEach(function(e) {
+    //     console.log("Edge " + e.v + " -> " + e.w + ": " + JSON.stringify(g.edge(e)));
+    // });
+    
+}
+
+function getNodes(tab, flow) {
+    return flow.filter(n => n.z === tab)
+}
+
+module.exports = layouter
---
+++ index.spec.js
@@ -0,0 +1,5 @@
+const layouter = require('.');
+
+const testflow = require('./fixtures/simple-flow.json');
+
+layouter(testflow);
